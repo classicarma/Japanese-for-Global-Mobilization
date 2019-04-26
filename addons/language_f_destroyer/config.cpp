558582cecdc0a2d0448a60d9803d7f06792dcd81
classicarma <skyburunave@gmail.com>
2019-04-26 15:56:38 +0900

    add new Japanese fonts add translation for the test

---
+++ addons/language_f_destroyer/config.cpp
@@ -0,0 +1,17 @@
+class CfgPatches
+{
+	class A3_Language_F_Destroyer_gmjpn
+	{
+		author="GM 日本語化";
+		name="CFGPATCHES_A3_Language_F_Destroyer_gmjpn";
+		url="https://github.com/classicarma/Japanese-for-Global-Mobilization";
+		requiredAddons[]=
+		{
+			"A3_Data_F_Destroyer",
+			"A3_Language_F_Destroyer"
+		};
+		requiredVersion=0.1;
+		units[]={};
+		weapons[]={};
+	};
+};
